f1c7cef76632cb36a0a5a777278ff019090aecaf
= <=>
2024-03-04 19:47:49 +0000

    .

--- stock_pick_algo.py
+++ stock_pick_algo.py
@@ -44,7 +44,7 @@ for ticker in sp500:
 data.insert(0, 'ticker', data.pop('ticker'))
 
 #filter data
-filtered_data = data[(data['marketCap'] > 20000000000) & (data['forwardPE'] < 20) & (data['priceToBook'] > 1) & (data['dividendYield'] > 0.025)]
+filtered_data = data[(data['marketCap'] > 2e10) & (data['forwardPE'] < 20) & (data['priceToBook'] > 1) & (data['dividendYield'] > 0.025)]
 
 #save filtered data df to csv
 filtered_data.to_csv('./OliverTurner_ProjectWork/Stock picking/filtered_data.csv')
